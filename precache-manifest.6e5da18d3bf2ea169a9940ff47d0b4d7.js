17b0bb34adcff5fca307e66ad320b16f291488aa
SuminDV <sumin.dmytro@gmail.com>
2019-12-16 11:59:12 +0200

    Updates

---
+++ precache-manifest.6e5da18d3bf2ea169a9940ff47d0b4d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad59887f7b5feed213f2b905bf56e97",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "dc49c7ca82cf472e3496",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.d208addd.chunk.css"
+  },
+  {
+    "revision": "b2defeb44b62643990fb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.514b795d.chunk.js"
+  },
+  {
+    "revision": "dc49c7ca82cf472e3496",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.be574fdb.chunk.js"
+  },
+  {
+    "revision": "11d7be2427f570e1e7d0",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.9a062077.js"
+  }
+]);
